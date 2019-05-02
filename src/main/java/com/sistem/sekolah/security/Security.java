6cce247014c8c291e7b5a1cf5398656536612b57
nanda <nandaceria@gmail.com>
2019-05-02 23:17:56 +0700

    Spring Security - In Memory

--- src/main/java/com/sistem/sekolah/security/Security.java
+++ src/main/java/com/sistem/sekolah/security/Security.java
@@ -1,41 +1,42 @@
 package com.sistem.sekolah.security;
 
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.builders.WebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-//@Configuration
-//@EnableWebSecurity
-//public class Security extends WebSecurityConfigurerAdapter {
-public class Security {
+@Configuration
+@EnableWebSecurity
+public class Security extends WebSecurityConfigurerAdapter {
 
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/login").permitAll().antMatchers("/admin/**")
-//				.authenticated().antMatchers("/siswa/**").authenticated().antMatchers("/guru/**").authenticated().and()
-//				.csrf().disable().formLogin().loginPage("/login").defaultSuccessUrl("/secure")
-//				.failureUrl("/login?error=true").usernameParameter("username").passwordParameter("password").and()
-//				.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/").and()
-//				.exceptionHandling().accessDeniedPage("/access-denied");
-//	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/login").permitAll().antMatchers("/admin/**")
+				.hasAnyRole("ADMIN").antMatchers("/siswa/**").authenticated().antMatchers("/guru/**").hasAnyRole("GURU")
+				.and().csrf().disable().formLogin().loginPage("/login").defaultSuccessUrl("/secure")
+				.failureUrl("/login?error=true").usernameParameter("username").passwordParameter("password").and()
+				.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/").and()
+				.exceptionHandling().accessDeniedPage("/access-denied");
+	}
 
-//	@Bean
-//	@Override
-//	protected UserDetailsService userDetailsService() {
-//		UserDetails user = User.withDefaultPasswordEncoder().username("1").password("1").roles("ROLE_USER").build();
-//		return new InMemoryUserDetailsManager(user);
-//	}
+	@Bean
+	@Override
+	protected UserDetailsService userDetailsService() {
+		User.UserBuilder users = User.withDefaultPasswordEncoder();
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		manager.createUser(users.username("guru").password("guru").roles("GURU").build());
+		manager.createUser(users.username("admin").password("admin").roles("ADMIN").build());
+		return manager;
+	}
 
-//	@Override
-//	public void configure(WebSecurity web) throws Exception {
-//		web.ignoring().antMatchers("/css/**", "/img/**", "/js/**", "/scss/**", "/vendor/**");
-//	}
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/css/**", "/img/**", "/js/**", "/scss/**", "/vendor/**");
+	}
 }
